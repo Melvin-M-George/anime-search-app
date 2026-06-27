509a6103ff16b9789c73703eadd3d5d10d67f706
Melvin-M-George <melvinmgeorge1234@gmail.com>
2026-06-27 17:31:09 +0530

    components added and css added

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,38 @@
 import React, { useEffect, useState } from 'react'
+import "./Components/style.css";
+import AnimeList from "./Components/AnimeList.jsx";
 
 export default function App() {
 
+  const [search, setSearch] = useState("Naruto");
   const [animeData, setAnimeData] = useState([]);
 
   const getData = async () => {
-    const response = await fetch("https://api.jikan.moe/v4/anime?q=naruto&limit=20");
+    const response = await fetch(`https://api.jikan.moe/v4/anime?q=${search}&limit=20`);
     const resData = await response.json();
     setAnimeData(resData.data);
   }
-
   useEffect(() => {
     getData();
-  }, [])
-
+  }, [search]);
   return (
     <>
       <div className="header">
         <h1>Anime List</h1>
         <div className="search-box">
-          <input type="search" placeholder="Search anime..." />
+          <input type="search" placeholder="Search anime..." onChange={(e)=>setSearch(e.target.value)} />
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="animeInfo">
+
+        </div>
+        <div className="anime-row">
+          <h2 className="text-heading">Anime</h2>
+          <div className="row">
+            <AnimeList></AnimeList>
+          </div>
         </div>
       </div>
     </>
